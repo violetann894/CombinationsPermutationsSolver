5e2bf9d7f3414d29cf9332f92c96fd65789b2306
violetann894 <rachelhuss2976@gmail.com>
2025-02-05 13:28:56 -0500

    Added tester objects and tested methods

--- src/CombinationsPermutationsTester.java
+++ src/CombinationsPermutationsTester.java
@@ -1,6 +1,28 @@
+import java.math.BigInteger;
+
+/**
+ *
+ * @author Rachel Hussmann
+ */
 public class CombinationsPermutationsTester {
 
     public static void main(String[] args) {
+
+        //
+        Combination cTester = new Combination();
+
+        //
+        Permutation pTester = new Permutation();
+
+        //
+        BigInteger n = BigInteger.valueOf(6);
+        BigInteger r = BigInteger.valueOf(2);
+
+        //
+        System.out.println(cTester.getCombinationEasy(6, 2));
+        System.out.println(cTester.getCombination(n, r));
+        System.out.println(pTester.getPermutationEasy(6, 2));
+        System.out.println(pTester.getPermutation(n, r));
 
     }
 
