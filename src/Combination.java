6070f414c302690fc84c729ee9c96285e2571957
violetann894 <rachelhuss2976@gmail.com>
2025-02-05 13:28:41 -0500

    Completed methods needed

--- src/Combination.java
+++ src/Combination.java
@@ -1,18 +1,92 @@
 import java.math.BigInteger;
 
+/**
+ *
+ * @author Rachel Hussmann
+ */
 public class Combination {
 
-    public int getCombinationEasy(int n, int c){
+    /**
+     *
+     * @param n
+     * @param r
+     * @return
+     */
+    public int getCombinationEasy(int n, int r){
 
+        //
+        int numerator = factorial(n);
 
+        //
+        int denominator = factorial(r)*factorial(n-r);
 
-        return 0;
+        //
+        return numerator/denominator;
     }
 
-    public BigInteger getCombination(BigInteger n, BigInteger c){
+    /**
+     *
+     * @param n
+     * @param r
+     * @return
+     */
+    public BigInteger getCombination(BigInteger n, BigInteger r){
 
+        //
+        BigInteger numerator = factorial(n);
 
+        //
+        BigInteger denominator = factorial(r).multiply(factorial(n.subtract(r)));
 
-        return ;
+        //
+        return numerator.divide(denominator);
+    }
+
+    /**
+     *
+     * @param i
+     * @return
+     */
+    public int factorial(int i){
+
+        //
+        int f = 1;
+
+        //
+        for(int j = 1; j <= i; j++){
+
+            //
+            f = f * j;
+        }
+
+        //
+        return f;
+    }
+
+    /**
+     *
+     * @param i
+     * @return
+     */
+    public BigInteger factorial(BigInteger i){
+
+        //
+        BigInteger f = BigInteger.valueOf(1);
+
+        //
+        BigInteger count = BigInteger.valueOf(1);
+
+        //
+        while(count.compareTo(i) < 1){
+
+            //
+            f = f.multiply(count);
+
+            //
+            count = count.add(BigInteger.valueOf(1));
+        }
+
+        //
+        return f;
     }
 }
--- src/Permutation.java
+++ src/Permutation.java
@@ -1,17 +1,93 @@
 import java.math.BigInteger;
 
+/**
+ *
+ * @author Rachel Hussmann
+ */
 public class Permutation {
 
-    public int getPermutationEasy(int n, int c){
+    /**
+     *
+     * @param n
+     * @param r
+     * @return
+     */
+    public int getPermutationEasy(int n, int r){
 
+        //
+        int numerator = factorial(n);
 
-        return 0;
+        //
+        int denominator = factorial(n-r);
+
+        //
+        return numerator/denominator;
     }
 
-    public BigInteger getPermutation(BigInteger n, BigInteger c){
+    /**
+     *
+     * @param n
+     * @param r
+     * @return
+     */
+    public BigInteger getPermutation(BigInteger n, BigInteger r){
 
+        //
+        BigInteger numerator = factorial(n);
 
+        //
+        BigInteger denominator = factorial(n.subtract(r));
 
-        return ;
+        //
+        return numerator.divide(denominator);
     }
+
+    /**
+     *
+     * @param i
+     * @return
+     */
+    public int factorial(int i){
+
+        //
+        int f = 1;
+
+        //
+        for(int j = 1; j <= i; j++){
+
+            //
+            f = f * j;
+        }
+
+        //
+        return f;
+    }
+
+    /**
+     *
+     * @param i
+     * @return
+     */
+    public BigInteger factorial(BigInteger i){
+
+        //
+        BigInteger f = BigInteger.valueOf(1);
+
+        //
+        BigInteger count = BigInteger.valueOf(1);
+
+        //
+        while (count.compareTo(i) < 1){
+
+            //
+            f = f.multiply(count);
+
+            //
+            count = count.add(BigInteger.ONE);
+        }
+
+        //
+        return f;
+    }
+
 }
