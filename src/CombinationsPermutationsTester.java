443511701f28b477c5abd9ab687cc3d043c35908
violetann894 <rachelhuss2976@gmail.com>
2025-02-05 16:24:09 -0500

    Added comments for clarity

--- src/CombinationsPermutationsTester.java
+++ src/CombinationsPermutationsTester.java
@@ -1,28 +1,30 @@
 import java.math.BigInteger;
 
 /**
- *
+ * The CombinationsPermutationsTester class is used to test the Combination and Permutation classes.
  * @author Rachel Hussmann
  */
 public class CombinationsPermutationsTester {
 
     public static void main(String[] args) {
 
-        //
+        //Created tester Combination object
         Combination cTester = new Combination();
 
-        //
+        //Created tester Permutation object
         Permutation pTester = new Permutation();
 
-        //
+        //Initializes some BigInteger objects for test cases
         BigInteger n = BigInteger.valueOf(6);
         BigInteger r = BigInteger.valueOf(2);
 
-        //
-        System.out.println(cTester.getCombinationEasy(6, 2));
+        //Testing simple example to make sure that factorial, getPermutation and getCombination methods work
+        System.out.println(cTester.getCombination(6, 2));
         System.out.println(cTester.getCombination(n, r));
-        System.out.println(pTester.getPermutationEasy(6, 2));
+        System.out.println(pTester.getPermutation(6, 2));
         System.out.println(pTester.getPermutation(n, r));
+
+
 
     }
 
